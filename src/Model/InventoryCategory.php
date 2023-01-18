19e0f3cafa98dc7289513eaf7393a45bdc823218
Libor Šebek <libor.sebek@religis.cz>
2023-01-18 10:38:08 +0100

    - add method getCata to Baselinker\Model\InventoryCategory

--- src/Model/InventoryCategory.php
+++ src/Model/InventoryCategory.php
@@ -6,6 +6,26 @@ class InventoryCategory
 {
 	public int $inventory_id;
 	public ?int $category_id;
-	public string $name;
+	public ?string $name;
 	public ?int $parent_id;
+
+	public  function getData(): array
+	{
+		$data = [
+			'inventory_id' => $this->inventory_id,
+		];
+		if(isset($this->category_id))
+		{
+			$data['category_id'] = $this->category_id;
+		}
+		if(isset($this->name))
+		{
+			$data['name'] = $this->name;
+		}
+		if(isset($this->parent_id))
+		{
+			$data['parent_id'] = $this->parent_id;
+		}
+		return $data;
+	}
 }
